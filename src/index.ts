f8728289c10bf2b78f64d42ea2c78dfbbe9e8e63
Jessica Kerr <jessitron@gmail.com>
2018-07-10 02:13:43 -0500

    top-level exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { LocalMachineConfig } from "./machine/LocalMachineConfig";
 export { newLocalSdm } from "./machine/newLocalSdm";
+export { runSlalom } from "./invocation/cli/runSlalom";
+export { runOnGitHook } from "./invocation/git/runOnGitHook";
