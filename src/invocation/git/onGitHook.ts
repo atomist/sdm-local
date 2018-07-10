7e824c385183baafc8f9de905e5cc1a2d201a9f1
Jessica Kerr <jessitron@gmail.com>
2018-07-10 02:12:51 -0500

    function for git hook

--- src/invocation/git/onGitHook.ts
+++ src/invocation/git/onGitHook.ts
@@ -1,29 +1,3 @@
-import { suppressConsoleLogging } from "../cli/support/configureLogging";
-import { logExceptionsToConsole, setCommandLineLogging } from "../cli/support/consoleOutput";
+import { runOnGitHook } from "./runOnGitHook";
 
-suppressConsoleLogging();
-setCommandLineLogging();
-
-import { logger } from "@atomist/automation-client";
-import { handleGitHookEvent } from "../../setup/gitHooks";
-import { localSdmInstance } from "../machineLoader";
-
-/**
- * Usage gitHookTrigger <git hook name> <directory> <branch> <sha>
- */
-
-const args = process.argv.slice(2);
-
-const event: string = args[0];
-const baseDir = args[1].replace("/.git/hooks", "");
-const branch = args[2];
-const sha = args[3];
-
-logger.info("Executing git hook against project at [%s], branch=%s, sha=%s",
-    baseDir, branch, sha);
-
-/* tslint:disable */
-
-logExceptionsToConsole(() =>
-    handleGitHookEvent(localSdmInstance, event, { baseDir, branch, sha }),
-);
+runOnGitHook(process.argv);
---
+++ src/invocation/git/runOnGitHook.ts
@@ -0,0 +1,31 @@
+import { suppressConsoleLogging } from "../cli/support/configureLogging";
+import { logExceptionsToConsole, setCommandLineLogging } from "../cli/support/consoleOutput";
+
+suppressConsoleLogging();
+setCommandLineLogging();
+
+import { logger } from "@atomist/automation-client";
+import { handleGitHookEvent } from "../../setup/gitHooks";
+import { localSdmInstance } from "../machineLoader";
+
+/**
+ * Usage gitHookTrigger <git hook name> <directory> <branch> <sha>
+ */
+
+export function runOnGitHook(argv: string[]) {
+    const args = argv.slice(2);
+
+    const event: string = args[0];
+    const baseDir = args[1].replace("/.git/hooks", "");
+    const branch = args[2];
+    const sha = args[3];
+
+    logger.info("Executing git hook against project at [%s], branch=%s, sha=%s",
+        baseDir, branch, sha);
+
+    /* tslint:disable */
+
+    logExceptionsToConsole(() =>
+        handleGitHookEvent(localSdmInstance, event, { baseDir, branch, sha }),
+    );
+}
