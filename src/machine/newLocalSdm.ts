8de7bae6677e874e6f9ea72de2f093de966948fa
Rod Johnson <rod@atomist.com>
2018-07-16 17:40:59 +1000

    Polishing

--- src/machine/newLocalSdm.ts
+++ src/machine/newLocalSdm.ts
@@ -3,6 +3,11 @@ import { LocalMachineConfig } from "./LocalMachineConfig";
 import { LocalSoftwareDeliveryMachine } from "./LocalSoftwareDeliveryMachine";
 import { mergeConfiguration } from "./mergeConfiguration";
 
+/**
+ * Create a new local SDM instance with the given configuration
+ * @param {LocalMachineConfig} config
+ * @return {LocalSoftwareDeliveryMachine}
+ */
 export function newLocalSdm(config: LocalMachineConfig): LocalSoftwareDeliveryMachine {
     const sdm = new LocalSoftwareDeliveryMachine(
         config.name,
