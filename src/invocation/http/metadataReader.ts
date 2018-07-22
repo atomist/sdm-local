f4b0d2fac2526689bf67c36f2fd332904f05fd4e
Rod Johnson <rod@atomist.com>
2018-07-22 17:42:45 +1000

    Better error handling on startup

--- src/invocation/http/metadataReader.ts
+++ src/invocation/http/metadataReader.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import { LocalMachineConfig } from "../..";
 import { AutomationClientInfo } from "../AutomationClientInfo";
 import { AutomationClientConnectionConfig } from "./AutomationClientConnectionConfig";
+import { errorMessage } from "../cli/support/consoleOutput";
 
 /**
  * Call into an SDM at the given location and retrieve metadata
@@ -9,17 +10,22 @@ import { AutomationClientConnectionConfig } from "./AutomationClientConnectionCo
  * @return {Promise<AutomationClientInfo>}
  */
 export async function getMetadata(connectionConfig: AutomationClientConnectionConfig): Promise<AutomationClientInfo> {
-    const resp = await axios.get(connectionConfig.baseEndpoint + "/registration");
-    const commandsMetadata = resp.data.commands;
-    let localConfig: LocalMachineConfig;
     try {
-        localConfig = (await axios.get(connectionConfig.baseEndpoint + "/localConfiguration")).data;
-    } catch {
-        // Do nothing
+        const resp = await axios.get(connectionConfig.baseEndpoint + "/registration");
+        const commandsMetadata = resp.data.commands;
+        let localConfig: LocalMachineConfig;
+        try {
+            localConfig = (await axios.get(connectionConfig.baseEndpoint + "/localConfiguration")).data;
+        } catch {
+            // Do nothing
+        }
+        return {
+            commandsMetadata,
+            localConfig,
+            connectionConfig,
+        };
+    } catch (e) {
+        errorMessage("Unable to connect to '%s': Is the automation client running?", connectionConfig.baseEndpoint);
+        process.exit(1);
     }
-    return {
-        commandsMetadata,
-        localConfig,
-        connectionConfig,
-    };
 }
