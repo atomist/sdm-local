ba1ecfadc2ee86b8f7d64e6690bb62ea055ae8f6
Rod Johnson <rod@atomist.com>
2018-07-25 08:27:41 +1000

    Break out HttpBuildStatusUpdater

---
+++ src/binding/HttpBuildStatusUpdater.ts
@@ -0,0 +1,46 @@
+import { BuildStatus, OnBuildComplete, SdmGoalEvent } from "@atomist/sdm";
+import { BuildStatusUpdater } from "@atomist/sdm-core/internal/delivery/build/local/LocalBuilder";
+import { invokeEventHandler } from "../invocation/http/EventHandlerInvocation";
+import { AutomationClientConnectionConfig } from "../invocation/http/AutomationClientConnectionConfig";
+
+/**
+ * Update build status by posting to an automation client
+ */
+export class HttpBuildStatusUpdater implements BuildStatusUpdater {
+
+    public async updateBuildStatus(rb, status, branch, buildNo, ctx) {
+        const goal: SdmGoalEvent = (ctx as any).trigger.data.SdmGoal[0];
+        const payload: OnBuildComplete.Subscription = {
+            Build: [{
+                buildId: buildNo,
+                status: status as BuildStatus,
+                commit: {
+                    sha: goal.push.after.sha,
+                    message: goal.push.after.message,
+                    repo: {
+                        name: goal.repo.name,
+                        owner: goal.repo.owner,
+                        channels: [{
+                            name: goal.repo.name,
+                            id: goal.repo.name,
+                            team: {
+                                id: ctx.teamId,
+                            },
+                        }],
+                    },
+                    statuses: [],
+                },
+                push: goal.push,
+            }],
+        };
+        const handlerNames = ["InvokeListenersOnBuildComplete"];
+        return Promise.all(handlerNames.map(name =>
+            invokeEventHandler(this.acc, {
+                name,
+                payload,
+            })));
+    }
+
+    constructor(private readonly acc: AutomationClientConnectionConfig) {
+    }
+}
--- src/machine/localLifecycle.ts
+++ src/machine/localLifecycle.ts
@@ -1,15 +1,9 @@
-import {
-    BuildStatus,
-    ExtensionPack,
-    OnBuildComplete,
-    SdmGoalEvent,
-    SoftwareDeliveryMachine,
-} from "@atomist/sdm";
+import { BuildStatus, ExtensionPack, SoftwareDeliveryMachine, } from "@atomist/sdm";
 import { BuildStatusUpdater } from "@atomist/sdm-core/internal/delivery/build/local/LocalBuilder";
 import { metadata } from "@atomist/sdm/api-helper/misc/extensionPack";
 import { DefaultAutomationClientConnectionConfig } from "../entry/resolveConnectionConfig";
-import { invokeEventHandler } from "../invocation/http/EventHandlerInvocation";
 import { isLocal } from "./isLocal";
+import { HttpBuildStatusUpdater } from "../binding/HttpBuildStatusUpdater";
 
 /**
  * Add Local IO to the given SDM.
@@ -21,39 +15,8 @@ export const LocalLifecycle: ExtensionPack = {
         if (isLocal()) {
             addLocalLifecycle(sdm);
             const bu = sdm as any as BuildStatusUpdater;
-            bu.updateBuildStatus = async (rb, status, branch, buildNo, ctx) => {
-                const goal: SdmGoalEvent = (ctx as any).trigger.data.SdmGoal[0];
-                const payload: OnBuildComplete.Subscription = {
-                    Build: [{
-                        buildId: buildNo,
-                        status: status as BuildStatus,
-                        commit: {
-                            sha: goal.push.after.sha,
-                            message: goal.push.after.message,
-                            repo: {
-                                name: goal.repo.name,
-                                owner: goal.repo.owner,
-                                channels: [{
-                                    name: goal.repo.name,
-                                    id: goal.repo.name,
-                                    team: {
-                                        id: ctx.teamId,
-                                    },
-                                }],
-                            },
-                            statuses: [],
-                        },
-                        push: goal.push,
-                    }],
-                };
-
-                const handlerNames = ["InvokeListenersOnBuildComplete"];
-                return Promise.all(handlerNames.map(name =>
-                    invokeEventHandler(DefaultAutomationClientConnectionConfig, {
-                        name,
-                        payload,
-                    })));
-            };
+            const buu = new HttpBuildStatusUpdater(DefaultAutomationClientConnectionConfig);
+            bu.updateBuildStatus = buu.updateBuildStatus.bind(buu);
         }
     },
 };
