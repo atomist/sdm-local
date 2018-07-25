00da2135da53ebb3fcab3f1e18bfc919f6693726
Rod Johnson <rod@atomist.com>
2018-07-26 09:28:26 +1000

    Remove old code

--- src/invocation/cli/support/WriteLineGoalDisplayer.ts
+++ src/invocation/cli/support/WriteLineGoalDisplayer.ts
@@ -1,27 +1,27 @@
-import { ExecuteGoalResult, Goal, Goals, OnPushToAnyBranch } from "@atomist/sdm";
-import { GoalDisplayer } from "./GoalDisplayer";
+import { ExecuteGoalResult, Goal, Goals, GoalsSetListenerInvocation, OnPushToAnyBranch } from "@atomist/sdm";
 
 import chalk from "chalk";
 
 /**
  * Display goals by writing each goal update to its own line on the console
  */
-export class WriteLineGoalDisplayer implements GoalDisplayer {
+export class WriteLineGoalDisplayer {
 
-    public displayGoalsSet(push: OnPushToAnyBranch.Push, goals: Goals) {
-        process.stdout.write(chalk.yellow(`▶ Goals for ${push.commits[0].sha}: ${goals.goals.map(g => chalk.italic(g.name)).join(" ⏦ ")}\n`));
-        process.stdout.write(`\t${chalk.italic(push.commits[0].message)}\n`);
+    public renderGoalsSet(gsi: GoalsSetListenerInvocation) {
+        const push = null;
+        return chalk.yellow(`▶ Goals for ${push.commits[0].sha}: ${gsi.goalSet.goals.map(g => chalk.italic(g.name)).join(" ⏦ ")}\n`) +
+            `\t${chalk.italic(push.commits[0].message)}\n`;
     }
 
     public displayGoalWorking(push: OnPushToAnyBranch.Push, goal: Goal, goals: Goals) {
-        process.stdout.write(chalk.yellow(`⚙︎ ${goal.inProcessDescription}\n`));
+        return chalk.yellow(`⚙︎ ${goal.inProcessDescription}\n`);
     }
 
     public displayGoalResult(push: OnPushToAnyBranch.Push, goal: Goal, ger: ExecuteGoalResult, goals: Goals) {
         if (ger.code !== 0) {
-            process.stdout.write(chalk.red(`✖︎︎ ${goal.failureDescription}\n`));
+            return chalk.red(`✖︎︎ ${goal.failureDescription}\n`);
         } else {
-            process.stdout.write(chalk.green(`✔ ${goal.successDescription}\n`));
+            return chalk.green(`✔ ${goal.successDescription}\n`);
         }
     }
 }
