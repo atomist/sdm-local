042ad27fac6cca1f6a409faea0c003f63fcd8a9d
Jessica Kerr <jessitron@gmail.com>
2018-07-25 15:41:26 -0500

    Remove all sync calls from expandedTreeRepoFinder

--- src/binding/expandedTreeRepoFinder.ts
+++ src/binding/expandedTreeRepoFinder.ts
@@ -2,6 +2,7 @@ import { logger } from "@atomist/automation-client";
 import { RepoFinder } from "@atomist/automation-client/operations/common/repoFinder";
 import * as fs from "fs";
 import * as _ from "lodash";
+import * as path from "path";
 import { promisify } from "util";
 import { FileSystemRemoteRepoRef } from "./FileSystemRemoteRepoRef";
 
@@ -12,16 +13,12 @@ import { FileSystemRemoteRepoRef } from "./FileSystemRemoteRepoRef";
  */
 export function expandedTreeRepoFinder(repositoryOwnerParentDirectory: string): RepoFinder {
     return async () => {
-        const eligibleDirectories: string[] =
-            _.flatten(await Promise.all((await promisify(fs.readdir)(repositoryOwnerParentDirectory))
-                .filter(item => fs.statSync(`${repositoryOwnerParentDirectory}/${item}`).isDirectory())
-                .map(async org => {
-                    logger.info("Searching under child directory [%s] of %s", org, repositoryOwnerParentDirectory);
-                    return (await promisify(fs.readdir)(`${repositoryOwnerParentDirectory}/${org}`))
-                        .map(repo => `${repositoryOwnerParentDirectory}/${org}/${repo}`)
-                        .filter(repo => fs.existsSync(`${repo}/.git`));
-                })),
-            );
+        const owners = await allDirectoriesUnder(repositoryOwnerParentDirectory);
+        logger.info("JESS FOUND OWNERS: " + owners);
+        const projects = await flatmapAsync(owners, allDirectoriesUnder);
+        const gitProjects = await filterAsync(projects, containsDirectory(".git"));
+        const eligibleDirectories = gitProjects;
+
         return eligibleDirectories.map(dir =>
             FileSystemRemoteRepoRef.fromDirectory({
                 repositoryOwnerParentDirectory,
@@ -31,3 +28,45 @@ export function expandedTreeRepoFinder(repositoryOwnerParentDirectory: string): 
             }));
     };
 }
+
+const readdir = promisify(fs.readdir);
+const filestat = promisify(fs.stat);
+
+/* returns the whole path to each directory under `parent` */
+async function allDirectoriesUnder(parent: string): Promise<string[]> {
+    const allFilenamesUnder: string[] = await readdir(parent);
+    const allPathsUnder = allFilenamesUnder.map(filename => path.join(parent, filename));
+    return filterAsync(allPathsUnder, pathIsDirectory);
+}
+
+async function pathIsDirectory(filepath: string): Promise<boolean> {
+    // will throw if filepath does not exist
+    return filestat(filepath).then(stat => stat.isDirectory());
+}
+
+async function flatmapAsync<T, U>(arr: T[], f: (t: T) => Promise<U[]>): Promise<U[]> {
+    return _.flatten(await Promise.all(arr.map(f)));
+}
+
+async function filterAsync<T>(arr: T[], f: (t: T) => Promise<boolean>): Promise<T[]> {
+    type WithTruthiness<TT> = [TT, boolean];
+
+    const enoughInfo: Array<WithTruthiness<T>> = await Promise.all(
+        arr.map(t => f(t)
+            .then((truthiness: boolean): WithTruthiness<T> => [t, truthiness])));
+
+    return enoughInfo.filter(wt => wt[1]).map(wt => wt[0]);
+}
+
+function containsDirectory(dirName: string): (parent: string) => Promise<boolean> {
+    return async (parentDir: string) => {
+        return (await filestat(path.join(parentDir, dirName))
+            .catch(() => NotADirectory)).isDirectory();
+    };
+}
+
+const NotADirectory = {
+    isDirectory() {
+        return false;
+    },
+};
