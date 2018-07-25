3f8a7ac214fc75e17aa9b85d1251f6b03cdf5655
Jessica Kerr <jessitron@gmail.com>
2018-07-25 14:08:55 -0500

    export infoMessage

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { runSlalom } from "./invocation/cli/runSlalom";
 export { runOnGitHook } from "./invocation/git/runOnGitHook";
 export { configureLocal } from "./machine/localPostProcessor";
 export { LocalLifecycle } from "./machine/localLifecycle";
+export { infoMessage } from "./invocation/cli/support/consoleOutput";
