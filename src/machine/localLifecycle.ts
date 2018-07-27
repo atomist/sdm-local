f67f5402b6d677dad5b7997e62e785da6582da89
Rod Johnson <rod@atomist.com>
2018-07-27 10:07:13 +1000

    Upgrade to new goal invocation

--- src/machine/localLifecycle.ts
+++ src/machine/localLifecycle.ts
@@ -1,9 +1,11 @@
-import { BuildStatus, ExtensionPack, SoftwareDeliveryMachine } from "@atomist/sdm";
+import { BuildStatus, ExtensionPack, SdmGoalState, SoftwareDeliveryMachine } from "@atomist/sdm";
 import { BuildStatusUpdater } from "@atomist/sdm-core/internal/delivery/build/local/LocalBuilder";
 import { metadata } from "@atomist/sdm/api-helper/misc/extensionPack";
 import { HttpBuildStatusUpdater } from "../binding/HttpBuildStatusUpdater";
 import { DefaultAutomationClientConnectionConfig } from "../entry/resolveConnectionConfig";
 import { isLocal } from "./isLocal";
+
+import chalk from "chalk";
 
 /**
  * Add Local IO to the given SDM.
@@ -21,9 +23,34 @@ export const LocalLifecycle: ExtensionPack = {
     },
 };
 
+/**
+ * Formatted for the console
+ * @param {SoftwareDeliveryMachine} sdm
+ */
 function addLocalLifecycle(sdm: SoftwareDeliveryMachine) {
     sdm.addPushImpactListener(async pu => {
         return pu.addressChannels(`Push to \`${pu.id.owner}:${pu.id.repo}\` - _${pu.commit.message}_`);
+    });
+    sdm.addGoalsSetListener(async gsi => {
+        return gsi.addressChannels(
+            chalk.yellow(`▶ Goals for ${gsi.push.commits[0].sha}: ${gsi.goalSet.goals.map(g => chalk.italic(g.name)).join(" ⏦ ")}\n`) +
+            `\t${chalk.italic(gsi.push.commits[0].message)}\n`);
+    });
+    sdm.addGoalCompletionListener(async gci => {
+        switch (gci.completedGoal.state) {
+            case SdmGoalState.success:
+                return gci.addressChannels(chalk.green(`✔ ${gci.completedGoal.description}\n`));
+            case SdmGoalState.requested:
+                return gci.addressChannels(chalk.red(`✖︎︎ ${gci.completedGoal.description}\n`));
+            //waiting_for_approval = "waiting_for_approval",
+            //failure = "failure",
+            // planned = "planned",
+            case SdmGoalState.in_process:
+                return gci.addressChannels(chalk.yellow(`⚙︎ ${gci.completedGoal.description}\n`));
+            //skipped = "skipped"
+            default:
+                break;
+        }
     });
     sdm.addBuildListener(async bu => {
         return bu.addressChannels(`Build status is \`${bu.build.status}\` ${buildStatusEmoji(bu.build.status)}`);
