8e07a9803625df7719c0637c7f4ab85f8d7af560
Rod Johnson <rod@atomist.com>
2018-08-05 11:33:38 +1000

    Fix #54

--- script/atomist-hook.sh
+++ script/atomist-hook.sh
@@ -5,14 +5,28 @@
 # Takes 5 arguments: JS script, git event, hook directory where the call came from,
 # branch and sha
 
+# Set ATOMIST_GITHOOK_VERBOSE to "true" to get verbose output
+
 script=$1
 event=$2
 hookDir=$3
 branch=$4
 sha=$5
 
-node $script\
-    $event \
-    $hookDir \
-    $branch\
-    $sha
+case "$ATOMIST_GITHOOK_VERBOSE" in
+ true)
+    node $script\
+       $event \
+       $hookDir \
+       $branch\
+       $sha \
+    ;;
+ *)
+    node $script\
+           $event \
+           $hookDir \
+           $branch\
+           $sha \
+           &>/dev/null &
+        ;;
+esac
