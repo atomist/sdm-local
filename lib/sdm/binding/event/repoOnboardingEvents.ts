6f342dd826f5d946b61bb868caf501ad2d6f762e
lievendoclo <lieven@atomist.com>
2018-09-14 13:39:19 +0200

    Unused import

--- lib/sdm/binding/event/repoOnboardingEvents.ts
+++ lib/sdm/binding/event/repoOnboardingEvents.ts
@@ -15,7 +15,7 @@
  */
 
 import { RepoId } from "@atomist/automation-client";
-import { CoreRepoFieldsAndChannels, OnRepoCreation } from "@atomist/sdm";
+import { OnRepoCreation } from "@atomist/sdm";
 import { OnChannelLink, OnRepoOnboarded } from "@atomist/sdm-core";
 import { repoFieldsFromProject } from "../../../common/git/pushFromLastCommit";
 import { EventSender } from "../../../common/invocation/EventHandlerInvocation";
