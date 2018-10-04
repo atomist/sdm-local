970e77bef8d89e893cf145d3881b5217fe696e67
Christian Dupuis <cd@atomist.com>
2018-10-04 13:01:23 +0200

    Delint

--- lib/sdm/configuration/support/NotifyOnStartupAutomationEventListener.ts
+++ lib/sdm/configuration/support/NotifyOnStartupAutomationEventListener.ts
@@ -22,18 +22,21 @@ import { AutomationClient } from "@atomist/automation-client/lib/automationClien
 import { codeLine } from "@atomist/slack-messages";
 import { newCliCorrelationId } from "../../../cli/invocation/http/support/newCorrelationId";
 
+/**
+ * Notify the local feed that an SDM is now connected and ready to receive traffic.
+ */
 export class NotifyOnStartupAutomationEventListener extends AutomationEventListenerSupport {
 
     public async startupSuccessful(client: AutomationClient): Promise<void> {
         const correlationId = await newCliCorrelationId({ channel: "general", encodeListenerPort: true });
-        const messageClient = await client.configuration.http.messageClientFactory({ context: { correlationId } } as any);
+        const messageClient = client.configuration.http.messageClientFactory({ context: { correlationId } } as any);
 
         const message = `__${client.configuration.sdm.name ? client.configuration.sdm.name : "SDM"}__ ${
             codeLine(`${client.configuration.name}:${client.configuration.version}`)}`;
 
         await messageClient.respond(`${message} is now connected`);
 
-         registerShutdownHook(async () => {
+        registerShutdownHook(async () => {
             await messageClient.respond(`${message} disconnected`);
             return 0;
         });
