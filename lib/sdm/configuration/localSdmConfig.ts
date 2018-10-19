1e4f19be6185ea521242c02d07de5b59282b87d5
Christian Dupuis <cd@atomist.com>
2018-10-19 15:13:46 +0200

    Add goal listener to shut down SDM after run

--- lib/sdm/configuration/localSdmConfig.ts
+++ lib/sdm/configuration/localSdmConfig.ts
@@ -14,13 +14,22 @@
  * limitations under the License.
  */
 
+import { logger } from "@atomist/automation-client";
 import {
     ExtensionPack,
+    GoalCompletionListener,
+    GoalCompletionListenerInvocation,
+    goalKeyString,
     metadata,
     onAnyPush,
+    SdmGoalEvent,
+    SdmGoalState,
     SoftwareDeliveryMachine,
 } from "@atomist/sdm";
-import { isInLocalMode } from "@atomist/sdm-core";
+import {
+    isGitHubAction,
+    isInLocalMode,
+} from "@atomist/sdm-core";
 
 /**
  * Extension pack that configures SDM for local
@@ -29,8 +38,11 @@ import { isInLocalMode } from "@atomist/sdm-core";
 export const LocalSdmConfig: ExtensionPack = {
     ...metadata("local-config"),
     configure: sdm => {
-        if (isInLocalMode()) {
+        if (isInLocalMode() || isGitHubAction()) {
             registerNoOpListeners(sdm);
+        }
+        if (isGitHubAction()) {
+            registerGoalSetListener(sdm);
         }
     },
 };
@@ -49,4 +61,33 @@ function registerNoOpListeners(sdm: SoftwareDeliveryMachine) {
         .addChannelLinkListener(NoOp);
     // Ensure there's a push mapping, even if it doesn't return anything
     sdm.addGoalContributions(onAnyPush().setGoals([]));
+}
+
+function registerGoalSetListener(sdm: SoftwareDeliveryMachine) {
+    sdm.addGoalCompletionListener(setGitHubStatusOnGoalCompletion());
+}
+
+export function setGitHubStatusOnGoalCompletion(): GoalCompletionListener {
+    return async (inv: GoalCompletionListenerInvocation) => {
+        const { id, completedGoal, allGoals } = inv;
+        logger.info("Completed goal: '%s' with '%s' in set '%s'",
+            goalKeyString(completedGoal), completedGoal.state, completedGoal.goalSetId);
+
+        if (completedGoal.state === SdmGoalState.failure) {
+            logger.info("Setting GitHub status to failed on %s" + id.sha);
+            process.exit(1);
+        }
+        if (allSuccessful(allGoals)) {
+            logger.info("Setting GitHub status to success on %s", id.sha);
+            process.exit(0);
+        }
+    };
+}
+
+function allSuccessful(goals: SdmGoalEvent[]): boolean {
+    goals.forEach(g => logger.debug("goal %s is %s", g.name, g.state));
+    return !goals.some(g =>
+        g.state !== SdmGoalState.success &&
+        g.state !== SdmGoalState.waiting_for_approval &&
+        g.state !== SdmGoalState.waiting_for_pre_approval);
 }
